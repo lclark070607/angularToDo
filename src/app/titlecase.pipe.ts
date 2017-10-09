f0c0e24eba3883f79b59cf07cb0bb28c2d0db484
Louise Clark <lclark070607@gmail.com>
2017-10-09 16:45:43 -0400

    adding a titlecase pipe

---
+++ src/app/titlecase.pipe.ts
@@ -0,0 +1,13 @@
+import { Pipe, PipeTransform } from '@angular/core';
+// creating a class decorated with pipe
+@Pipe({
+  name: 'titlecase'
+})
+export class TitlecasePipe implements PipeTransform {
+  // ? is an optional parameter,
+  transform(value: any, args?: any): any {
+    return value.charAt(0).toUpperCase()
+         + value.substr(1).toLowerCase();
+  }
+
+}
