82e74a7d71524e93ae1f506e1a2b28072b445cee
Louise Clark <lclark070607@gmail.com>
2017-10-09 15:03:23 -0400

    making list display multiple items

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,20 @@
 import { Component } from '@angular/core';
 
+//Decorator:  modify classes as they're being compiled
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: 'app-root', //tells Angular go and find something that it's going to fill in in the app-root selector
+  templateUrl: './app.component.html', //tells Angular where to go and get the html
+  styleUrls: ['./app.component.css'] //what css files we want to apply
 })
+//Typescript Class
 export class AppComponent {
-  title = 'app';
+//member variable
+  title = 'Things To Do';
+
+  thingsToDo = [
+    'Learn Javascript',
+    'Learn Typescript',
+    'Learn Angular',
+    'Learn Redux'
+  ];
 }
