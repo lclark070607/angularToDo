73d541b1f3d36389d4a95e73cede116d3b82bb6f
Louise Clark <lclark070607@gmail.com>
2017-10-10 13:24:29 -0400

    added redirect

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,5 +2,7 @@ import { Routes } from '@angular/Router';
 import { ToDoPageComponent } from './to-do-page/to-do-page.component';
 
 export const routeConfig: Routes = [
+    // Whenever the path is empty, automatically redirect the user to the todo path
+    { path: '', redirectTo: 'todo', pathMatch: 'full'},
     {path: 'todo', component: ToDoPageComponent }
 ]
